227c43df6c06fda0eba7cfe28bcdbbf08a792dcd
tapza007 <tanudechakul@students.mags.school.nz>
2026-06-25 09:49:14 +1200

    New map test

--- user interface/pause_menu.gd
+++ user interface/pause_menu.gd
@@ -21,15 +21,15 @@ func _process(_delta: float) -> void:
 	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused:
 		resume()
 
-
+# resume button
 func _on_resume_button_up() -> void:
 	resume()
 
-
+# restart button
 func _on_restart_button_up() -> void:
 	get_tree().reload_current_scene()
 	
 
-
+# main menu button
 func _on_quit_pressed() -> void:
 	get_tree().quit()
