4d325fbc352e403c74fc5c8066f072e2d8f2c235
tapza007 <tanudechakul@students.mags.school.nz>
2026-08-21 13:26:44 +1200

    New map and enermy system

--- user interface/dead_menu.gd
+++ user interface/dead_menu.gd
@@ -2,3 +2,4 @@ extends Control
 
 func _ready() -> void:
 		$Camera2D.screen_shake(10 , 1)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
--- user interface/main_menu.gd
+++ user interface/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+
 func _on_exit_pressed() -> void:
 	get_tree().quit()
